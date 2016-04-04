65bbdc3fd9ba6963ac53dc48fe9c651ae3fb42de
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-04 14:25:06 +0800

    null

---
+++ LALR1Compiler/LexicalAnalyzer.Fixed.Null.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LALR1Compiler
+{
+    /// <summary>
+    /// 词法分析器的抽象基类。对一个字符串进行词法分析
+    /// </summary>
+    public abstract partial class LexicalAnalyzer
+        : ILexicalAnalyzer
+    {
+
+        // null是一个特殊的关键字，他代表“没有结点”
+
+    }
+
+}
