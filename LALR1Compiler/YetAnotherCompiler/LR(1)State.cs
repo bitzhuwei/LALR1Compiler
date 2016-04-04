da21449d7b12dcd6268a364612987a0fb32329d8
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-04 13:27:20 +0800

    SmallerLR1State

--- LALR1Compiler/YetAnotherCompiler/LR(1)State.cs
+++ LALR1Compiler/YetAnotherCompiler/LR(1)State.cs
@@ -31,4 +31,191 @@ namespace LALR1Compiler
         }
 
     }
+
+    /// <summary>
+    /// 占用内存更少的LR1State
+    /// </summary>
+    public class SmallerLR1State : HashCache, IEnumerable<LR1Item>
+    {
+        class LR1ItemGroup : HashCache
+        {
+            public LR0Item Item { get; private set; }
+            public OrderedCollection<TreeNodeType> LookAheadNodeList { get; private set; }
+
+            public LR1ItemGroup(Regulation regulation, int dotPosition, params TreeNodeType[] lookAheadNodes)
+                : base(GetUniqueString)
+            {
+                this.Item = new LR0Item(regulation, dotPosition);
+                this.LookAheadNodeList = new OrderedCollection<TreeNodeType>(", ");
+                foreach (var node in lookAheadNodes)
+                {
+                    this.LookAheadNodeList.TryInsert(node);
+                }
+            }
+            private static string GetUniqueString(HashCache cache)
+            {
+                LR1ItemGroup obj = cache as LR1ItemGroup;
+                return obj.Dump();
+            }
+
+            public override void Dump(System.IO.TextWriter stream)
+            {
+                this.Item.Dump(stream);
+                stream.Write(", ");
+                this.LookAheadNodeList.Dump(stream);
+            }
+
+            public override int CompareTo(HashCache other)
+            {
+                LR1ItemGroup obj = other as LR1ItemGroup;
+                if ((Object)obj == null) { return -1; }
+                return this.Item.CompareTo(obj.Item);
+            }
+        }
+        private OrderedCollection<LR1ItemGroup> list = new OrderedCollection<LR1ItemGroup>(Environment.NewLine);
+
+        /// <summary>
+        /// 这是一个只能添加元素的集合，其元素是有序的，是按二分法插入的。
+        /// 但是使用者不能控制元素的顺序。
+        /// </summary>
+        /// <param name="separator">在Dump到流时用什么分隔符分隔各个元素？</param>
+        public SmallerLR1State()
+            : base(GetUniqueString)
+        {
+        }
+
+        public override int CompareTo(HashCache other)
+        {
+            return base.CompareTo(other);
+        }
+
+        private static string GetUniqueString(HashCache cache)
+        {
+            SmallerLR1State obj = cache as SmallerLR1State;
+            return obj.Dump();
+            //StringBuilder builder = new StringBuilder();
+            //for (int i = 0; i < obj.list.Count; i++)
+            //{
+            //    builder.Append(obj.list[i]);
+            //    if (i + 1 < obj.list.Count)
+            //    {
+            //        builder.Append(obj.seperator);
+            //    }
+            //}
+
+            //return builder.ToString();
+        }
+
+        public bool TryInsert(LR1Item item)
+        {
+            var fakeGroup = new LR1ItemGroup(item.Regulation, item.DotPosition);
+            if (this.list.TryInsert(fakeGroup))
+            {
+                this.SetDirty();
+                return true;
+            }
+            else
+            {
+                int index = this.list.IndexOf(fakeGroup);
+                LR1ItemGroup group = this.list[index];
+                if (group.LookAheadNodeList.TryInsert(item.LookAheadNodeType))
+                {
+                    this.SetDirty();
+                    return true;
+                }
+                else
+                { return false; }
+            }
+        }
+
+        public int IndexOf(LR1Item item)
+        {
+            int index = 0;
+            foreach (var group in this.list)
+            {
+                if (group.Item.Regulation == item.Regulation
+                    && group.Item.DotPosition == item.DotPosition)
+                {
+                    foreach (var node in group.LookAheadNodeList)
+                    {
+                        if (node == item.LookAheadNodeType)
+                        {
+                            return index;
+                        }
+
+                        index++;
+                    }
+                }
+
+                index += group.LookAheadNodeList.Count;
+            }
+
+            return -1;
+        }
+
+        public bool Contains(LR1Item item)
+        {
+            var fakeGroup = new LR1ItemGroup(item.Regulation, item.DotPosition);
+            int index = this.list.IndexOf(fakeGroup);
+            return (0 <= index && index < this.list.Count);
+        }
+
+        public LR1Item this[int index]
+        {
+            get
+            {
+                if (index < 0) { throw new ArgumentOutOfRangeException(); }
+
+                int i = 0;
+                foreach (var group in this.list)
+                {
+                    foreach (var node in group.LookAheadNodeList)
+                    {
+                        if (i == index)
+                        {
+                            return new LR1Item(
+                                group.Item.Regulation, group.Item.DotPosition, node);
+                        }
+                        else
+                        { i++; }
+                    }
+                }
+
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public IEnumerator<LR1Item> GetEnumerator()
+        {
+            foreach (var group in this.list)
+            {
+                foreach (var node in group.LookAheadNodeList)
+                {
+                    yield return new LR1Item(
+                        group.Item.Regulation, group.Item.DotPosition, node);
+                }
+            }
+        }
+
+        public int GroupCount { get { return this.list.Count; } }
+
+        public int Count { get { return this.list.Sum(x => x.LookAheadNodeList.Count); } }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override void Dump(System.IO.TextWriter stream)
+        {
+            for (int i = 0; i < this.list.Count; i++)
+            {
+                this.list[i].Dump(stream);
+                if (i + 1 < this.list.Count)
+                {
+                    stream.Write(Environment.NewLine);
+                }
+            }
+        }
+    }
 }
