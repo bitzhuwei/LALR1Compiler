9229de59f080d82830828198ec6ec22f8602e502
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-16 12:04:12 +0800

    external_declaration

--- GLSLParser/GLSLParserSyntaxTree.DumpShaderDeclarations.cs
+++ GLSLParser/GLSLParserSyntaxTree.DumpShaderDeclarations.cs
@@ -18,24 +18,65 @@ namespace GLSLParser
             if (syntaxTree.Children.Count == 1)
             {
                 // <translation_unit> ::= <external_declaration> ;
-                ExternalDeclaration(syntaxTree.Children[0], result);
+                external_declaration(syntaxTree.Children[0], result);
             }
             else if (syntaxTree.Children.Count == 2)
             {
                 // <translation_unit> ::= <translation_unit> <external_declaration> ;
-                TranslationUnit(syntaxTree.Children[0], result);
-                ExternalDeclaration(syntaxTree.Children[1], result);
+                translation_unit(syntaxTree.Children[0], result);
+                external_declaration(syntaxTree.Children[1], result);
             }
+            else
+            { throw new Exception(); }
 
             return result;
         }
 
-        private static void TranslationUnit(SyntaxTree syntaxTree, List<Declaration> result)
+        private static void translation_unit(SyntaxTree syntaxTree, List<Declaration> result)
+        {
+            if (syntaxTree.NodeType.Type != GLSLTreeNodeType.NODE__translation_unit)
+            { throw new ArgumentException(); }
+
+            if (syntaxTree.Children.Count == 1)
+            {
+                // <translation_unit> ::= <external_declaration> ;
+                translation_unit(syntaxTree.Children[0], result);
+            }
+            else if (syntaxTree.Children.Count == 2)
+            {
+                // <translation_unit> ::= <translation_unit> <external_declaration> ;
+                translation_unit(syntaxTree.Children[0], result);
+                external_declaration(syntaxTree.Children[1], result);
+            }
+            else
+            { throw new Exception(); }
+        }
+
+        private static void external_declaration(SyntaxTree syntaxTree, List<Declaration> result)
+        {
+            if (syntaxTree.NodeType.Type != GLSLTreeNodeType.NODE__external_declaration)
+            { throw new ArgumentException(); }
+
+            if (syntaxTree.Children[0].NodeType.Type == GLSLTreeNodeType.NODE__function_definition)
+            {
+                // <external_declaration> ::= <function_definition> ;
+                function_definition(syntaxTree.Children[0], result);
+            }
+            else if (syntaxTree.Children[0].NodeType.Type == GLSLTreeNodeType.NODE__declaration)
+            {
+                // <external_declaration> ::= <declaration> ;
+                declaration(syntaxTree.Children[0], result);
+            }
+            else
+            { throw new Exception(); }
+        }
+
+        private static void declaration(SyntaxTree syntaxTree, List<Declaration> result)
         {
             throw new NotImplementedException();
         }
 
-        private static void ExternalDeclaration(SyntaxTree syntaxTree, List<Declaration> result)
+        private static void function_definition(SyntaxTree syntaxTree, List<Declaration> result)
         {
             throw new NotImplementedException();
         }
