1b6919a6ade212e9abc08a1993e8008f5b6974c0
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-16 11:50:25 +0800

    GLSLPreprocessor

---
+++ GLSLParser/GLSLPreprocessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLSLParser
+{
+    public static class GLSLPreprocessor
+    {
+        /// <summary>
+        /// 预处理。去掉#version #if等指令
+        /// </summary>
+        /// <param name="sourceCode"></param>
+        /// <returns></returns>
+        public static string Preprocess(this string sourceCode)
+        {
+            //TODO:
+            throw new NotImplementedException();
+        }
+    }
+}
