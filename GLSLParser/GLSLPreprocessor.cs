443ab6174f9dc5dba45e66fe6534b4cd897b6b02
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-18 17:50:03 +0800

    DummyPreprocess

--- GLSLParser/GLSLPreprocessor.cs
+++ GLSLParser/GLSLPreprocessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,53 @@ namespace GLSLParser
     public static class GLSLPreprocessor
     {
         /// <summary>
-        /// 预处理。去掉#version #if等指令
+        /// 去掉行尾可能出现的'\\'
+        /// </summary>
+        /// <param name="fullname"></param>
+        /// <returns></returns>
+        public static string ConnectLines(this string fullname)
+        {
+            var builder = new StringBuilder();
+            using (StreamReader sr = new StreamReader(fullname))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line.EndsWith("\\"))
+                    {
+                        int length = line.Length;
+                        builder.Append(line.Substring(0, length - 1));
+                    }
+                    else
+                    {
+                        builder.AppendLine(line);
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+        /// <summary>
+        /// 预处理。去掉所有的#指令。
         /// </summary>
         /// <param name="sourceCode"></param>
         /// <returns></returns>
-        public static string Preprocess(this string sourceCode)
+        public static string DummyPreprocess(this string fullname)
         {
-            //TODO:
-            throw new NotImplementedException();
+            StringBuilder builder = new StringBuilder();
+            using (StreamReader sr = new StreamReader(fullname))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (line.Trim().StartsWith("#")) { continue; }
+
+                    builder.AppendLine(line);
+                }
+            }
+
+            return builder.ToString();
         }
     }
 }
